2e9ba5ae0803f4acc0858fc1f56f7b7928d0ff11
darrell404 <darrell029@gmail.com>
2024-02-19 02:20:28 +0000

    add board component unit test

--- src/tests/Board.spec.ts
+++ src/tests/Board.spec.ts
@@ -6,11 +6,13 @@ import { useStore } from "../store"
 import { createVuetify } from "vuetify"
 import { DifficultyLevel } from "../utils/constants"
 import { flushPromises } from "@vue/test-utils"
+import { navigateNeighbours } from "../utils/helpers"
+
+vi.mock("../utils/helpers")
 
 describe("Board", () => {
-  const pinia = createTestingPinia()
+  const pinia = createTestingPinia({ stubActions: false })
   const vuetify = createVuetify()
-  const store = useStore()
 
   function wrapperFactory() {
     return shallowMount(Board, {
@@ -26,6 +28,7 @@ describe("Board", () => {
   }
 
   it("sets up the board", async () => {
+    const store = useStore()
     const wrapper = wrapperFactory()
     store.game = DifficultyLevel.MEDIUM
     const setupSpy = vi.spyOn(wrapper.vm, "setupGame")
@@ -38,5 +41,26 @@ describe("Board", () => {
     expect(createMatrixSpy).toHaveBeenCalledTimes(1)
     expect(plotBombsSpy).toHaveBeenCalledTimes(1)
     expect(store.setBoardData).toHaveBeenCalled()
+
+    expect(store.boardData.length).toBe(8)
+    expect(store.boardData[0].length).toBe(12)
+    expect(store.mines.length).toBe(20)
+  })
+
+  it("does not trigger show all mines if first click has a mine", async () => {
+    const store = useStore()
+    const wrapper = wrapperFactory()
+    const bombCheckerSpy = vi.spyOn(wrapper.vm, "bombChecker")
+    const showMinesSpy = vi.spyOn(wrapper.vm, "showMines")
+    store.boardData[0][0].hasMine = true
+
+    expect(wrapper.vm.firstPlotClicked).toBe(false)
+
+    wrapper.vm.clickEventHandler(0, 0)
+    await flushPromises()
+
+    expect(wrapper.vm.firstPlotClicked).toBe(true)
+    expect(showMinesSpy).not.toHaveBeenCalled()
+    expect(bombCheckerSpy).toHaveBeenCalled()
   })
 })
